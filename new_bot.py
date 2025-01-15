72080de0b72604b47fd9bf741627ddcb1c48a81a
Grigory <lololohka057@gmail.com>
2025-01-15 18:54:17 +0300

    save

--- new_bot.py
+++ new_bot.py
@@ -3075,6 +3075,10 @@ async def generate_pdf_and_send(update, context, full_text, exact_title):
     file_name = f"{user_id}_{unique_title}.pdf"
     file_path = f"media/{file_name}"
 
+    # Проверяем, существует ли каталог 'media', если нет — создаем
+    if not os.path.exists('media'):
+        os.makedirs('media')
+        
     # Сохраняем PDF
     pdf.output(file_path)
 
